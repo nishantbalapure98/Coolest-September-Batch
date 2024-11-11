c0f5a6fbd02d4ce62b44d92c2922a092570f3b0f
nishantbalapure98 <piyushbalapure98@gmail.com>
2024-11-11 21:38:48 +0530

    new changes

--- Class-13 (Async Programming-1)/script.js
+++ Class-13 (Async Programming-1)/script.js
@@ -7,19 +7,35 @@
 
 // console.log('End')
 
-console.log('Start')
+// console.log('Start')
 
-function sayHello(){
-    console.log("Hello")
-} // 2-3 seconds
-
-
-function sayBye(){
-    console.log('Bye')
-}
+// function sayHello(){
+//     console.log("Hello")
+// } // 2-3 seconds
 
 
-setTimeout(sayHello , 3000)
-setTimeout(sayBye , 3000)
-console.log('End')
+// function sayBye(){
+//     console.log('Bye')
+// }
 
+
+// setTimeout(sayHello , 3000)
+// setTimeout(sayBye , 3000)
+// console.log('End')
+
+// Import the 'fs' module
+const fs = require("fs");
+
+console.log('Before'); // This will be printed first
+
+// Read file asynchronously (non-blocking)
+fs.readFile('f1.txt', (err, data) => {
+  if (err) {
+    console.error('An error occurred:', err);
+    return;
+  }
+  
+  console.log('This is File 1 data -> ' + data); // This is printed when the file is read
+});
+
+console.log('After'); // This will be printed before the file is read
