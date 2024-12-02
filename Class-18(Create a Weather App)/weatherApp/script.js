0a96e7dce10dea770285aa166e299c04f2e7afb9
nishantbalapure98 <piyushbalapure98@gmail.com>
2024-12-02 21:45:27 +0530

    new

--- Class-18(Create a Weather App)/weatherApp/script.js
+++ Class-18(Create a Weather App)/weatherApp/script.js
@@ -1,47 +1,40 @@
-const form = document.querySelector("form");
+const temperatureField = document.querySelector(".temp");
+const cityField = document.querySelector(".time_location p");
+const dateField = document.querySelector(".time_location span");
+const emojiField = document.querySelector(".weather_condition img");
+const weatherField = document.querySelector(".weather_condition span");
 const searchField = document.querySelector(".searchField");
-const currentLocation = document.querySelector('.time_location p')
-const dateTime = document.querySelector('.time_location span')
-const temprature = document.querySelector('.temp')
-const conditionField = document.querySelector('.weather_condition span')
+const form = document.querySelector("form");
 
+let target = "Pune"
+async function fetchData(target){
+    try {
+        let url = `https://api.weatherapi.com/v1/current.json?key=8b6d5f63a04a485fa5351525232908&q=${target}&aqi=no`
 
-form.addEventListener("submit", search);
+        const response = await fetch(url)
 
-function search(e) {
-  e.preventDefault();
-  fetchData(searchField.value);
+        const data = await response.json()
+
+        form.addEventListener('submit' , search )
+
+        function search(e){
+
+          e.preventDefault()
+          target = searchField.value
+      
+          fetchData(target)
+      }
+        console.log(data)
+
+        let currentTemp = data.current.temp_c
+        let currentCondition = data.current.condition.text
+        let locationName = data.location.name
+        let localTime = data.location.localtime
+        let conditionEmoji = data.current.condition.icon
+        console.log(currentTemp ,currentCondition ,locationName , localTime , conditionEmoji )
+    }
+    catch(error){
+        console.log(error)
+    }
 }
-
-async function fetchData(target) {
-  try {
-    const url = `http://api.weatherapi.com/v1/current.json?key=35af7ff606db422880d141328231305&q=${target}&aqi=yes`;
-
-    // fetch
-    const respone = await fetch(url);
-
-    const data = await respone.json();
-
-     let currentLocation = data.location.name
-     let currentCondition = data.current.condition.text
-     let currentTemp = data.current.temp_c
-     let currentTime = data.location.localtime
-
-     updateValues(currentTime , currentLocation ,currentCondition , currentTemp )
-
-    
-    console.log(data);
-  } catch (error) {
-    console.error("Cannot Fetch");
-  }
-}
-
-function updateValues(time , location , condition , temp){
-     dateTime.innerText = time
-     currentLocation.innerText = location
-     conditionField.innerText = condition
-     temprature.innerText = temp
-
-}
-
-fetchData("Mumbai");
+fetchData(target)
